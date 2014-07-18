93d514da3298452ef4c3d88716fa3d759ce213f6
Amir Kanpurwala <akanpurwala@gmail.com>
2014-07-18 15:46:28 -0700

    Configured app for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # http://amir-bloccit.herokuapp.com
+  config.action_mailer.default_url_options = {:host => 'amir-bloccit.herokuapp.com'}
+
 end
