690b61c0db944bedc0e7c991d58ea0fe6110663d
Amir Kanpurwala <akanpurwala@gmail.com>
2014-07-17 17:32:11 -0700

    Added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Bloccit::Application.routes.draw do
+  devise_for :users do
+    delete 'logout' => 'sessions#destroy', :as => :destroy_user_session
+  end
+
   resources :posts
   
   match "about" => 'welcome#about', via: :get
