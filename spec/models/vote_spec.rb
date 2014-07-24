489349df4915ed71110624691350f5f105e88c09
Amir Kanpurwala <akanpurwala@gmail.com>
2014-07-24 16:41:05 -0700

    Implemented testing suite and created tests for Vote

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,36 +1,35 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe Vote do
-  
+RSpec.describe Vote, :type => :model do
+
   describe "#up_vote?" do
     it "returns true for an up vote" do
       v = Vote.new(value: 1)
-      v.up_vote?.should be_true
+      expect(v.up_vote?).to be_truthy
     end
     it "returns false for a down vote" do
       v = Vote.new(value: -1)
-      v.up_vote?.should be_false
+      expect(v.up_vote?).to be_falsy
     end
   end
 
   describe "#down_vote?" do
     it "returns true for a down vote" do
       v = Vote.new(value: -1)
-      v.down_vote?.should be_true
+      expect(v.down_vote?).to be_truthy
     end
     it "returns false for an up vote" do
       v = Vote.new(value: 1)
-      v.down_vote?.should be_false
+      expect(v.down_vote?).to be_falsy
     end
   end
 
   describe "#update_post" do
-    it "calls `update_rank` on post" do
+    it "calls 'update_rank' on post" do
       post = create(:post)
-      post.should respond_to(:update_rank)
-      post.should_receive(:update_rank)
+      expect(post).to respond_to(:update_rank)
+      expect(post).to receive(:update_rank)
       Vote.create(value: 1, post: post)
     end
   end
-
 end
